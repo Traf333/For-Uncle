a58e5ec2d9e667e46aa464918a1dd7282b5637b7
Igor Trofimov <igortr333@gmail.com>
2012-12-17 20:24:11 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForUncle::Application.config.secret_token = '50be0d5bc6345d26e07ba66779f4b46144775e72fbf715aca5faade7420bb658580e2121ee6114063ea1cbd1a9e13e94f983c0a4be40fd8d2ba9b905b6446557'
