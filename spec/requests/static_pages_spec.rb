f8429bc61a45f2059ae25d5882ee4d3885265124
Igor Trofimov <igortr333@gmail.com>
2012-12-21 15:53:55 +0300

    Add a title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,29 +2,37 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "For Uncle" }
+
   describe "Home pages" do
 
-    it "should have the content 'For Uncle'" do
+    it "should have the h1 'For Uncle'" do
       visit '/static_pages/home'
-      page.should have_selector('For Uncle')
+      page.should have_selector('h1', :text => 'For Uncle')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => " | Home")
+      page.should have_selector('title', :text => "#{base_title}")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+
   end
 
   describe "Help pages" do
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('Help')
+      page.should have_selector('h1', :text=> 'Help')
     end
 
      it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => " | Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
@@ -32,12 +40,12 @@ describe "StaticPages" do
 
     it "should have the content 'About'" do
       visit '/static_pages/about'
-      page.should have_selector('About')
+      page.should have_selector('h1', :text => 'About')
     end
 
      it "should have the title 'About Me'" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => " | About Me")
+      page.should have_selector('title', :text => "#{base_title} | About Me")
     end
   end
 end
