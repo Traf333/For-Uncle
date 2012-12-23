3b86a8a91bee0500a1575fdb4ce5a2b8f539ecbd
Igor Trofimov <igortr333@gmail.com>
2012-12-23 18:10:25 +0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 ForUncle::Application.routes.draw do
-  get "static_pages/home"
+  get "clients/new"
 
-  get "static_pages/help"
+  root :to => 'static_pages#home'
 
-  get "static_pages/about"
+  match 'signup', to: 'clients#new'
+  match '/help', to: 'static_pages#help'
 
+  match '/about', to: 'static_pages#about'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -54,7 +56,7 @@ ForUncle::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,51 +1,28 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
 
-  let(:base_title) { "For Uncle" }
+  subject { page }
 
-  describe "Home pages" do
+  describe "Home page" do
+    before { visit root_path }
 
-    it "should have the h1 'For Uncle'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'For Uncle')
-    end
-
-    it "should have the title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "#{base_title}")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
-    end
-
+    it { should have_selector('h1',    text: 'For Uncle') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
   end
 
-  describe "Help pages" do
+  describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text=> 'Help')
-    end
-
-     it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "#{base_title} | Help")
-    end
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
-  describe "About pages" do
+  describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About')
-    end
-
-     it "should have the title 'About Me'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "#{base_title} | About Me")
-    end
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
 end
