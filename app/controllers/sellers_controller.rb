17b267a79b8bc6e6e8a709cfbedcc7b027f28496
Igor Trofimov <igortr333@gmail.com>
2013-01-08 12:02:51 +0300

    Add edit, update

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,5 +1,8 @@
 class SellersController < ApplicationController
   
+  before_filter :signed_in_seller, only: [:edit, :update]
+  before_filter :correct_seller,   only: [:edit, :update]
+
   def index
     @sellers = Seller.all
   end
@@ -23,10 +26,36 @@ class SellersController < ApplicationController
     end
   end
 
+  def edit
+    @seller = Seller.find(params[:id])
+  end
+
+  def update
+    @seller = Seller.find(params[:id])
+    if @seller.update_attributes(params[:seller])
+      sign_in @seller
+      flash[:success] = "Profile updated"
+      redirect_to @seller
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     @seller = Seller.find(params[:id])
     @seller.destroy
 
     redirect_to sellers_url
   end
+
+  private
+
+    def signed_in_seller
+      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_seller
+      @seller = Seller.find(params[:id])
+      redirect_to root_path unless current_seller?(@seller)
+    end
 end
